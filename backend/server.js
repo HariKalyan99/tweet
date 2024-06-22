0dbf1a65d79c96db75cf6dd609eab302fb39a3c7
HariKalyan99 <harikalyan99tronics@gmail.com>
2024-06-22 18:03:59 +0530

    for prod--

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+dotenv.config();
+import path from 'path';
 import {v2 as cloudinary} from 'cloudinary'
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -7,7 +9,6 @@ import userRoutes from './routes/user.routes.js'
 import postRoutes from './routes/post.routes.js'
 import notificationRoutes from './routes/notification.routes.js'
 import connectToMongo from './db/connectMongoDb.js';
-dotenv.config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -18,12 +19,23 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 8082
 
+const __dirname = path.resolve()
+
 app.use(express.json({limit: '5mb'}))
 app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (request, response) => {
+        response.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
